2b6b9eb2908535443083e0251a08a7f286e9d005
jav1ts <55659287+jav1ts@users.noreply.github.com>
2020-02-22 17:22:21 -0500

    added AJAX call to capture user inpputted address to targetthe latitude and longitude from the mapquest API

--- public/js/customerLogic.js
+++ public/js/customerLogic.js
@@ -1,3 +1,36 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $("#nav-placeholder").load("nav.html");
+    $("#addressSubmit").on("click", function (event) {
+        event.preventDefault();
+
+        // Grabbing and storing the data-animal property value from the button
+        var address = $("#userAddress").val().trim();
+        address = address.split(" ");
+        address = address.join("+");
+        console.log(address);
+
+        // console.log(address);
+
+        // Constructing a queryURL using the address captured from the Address input field in the HTML
+        var queryURL = "http://open.mapquestapi.com/geocoding/v1/address?key=DTPAAe2yk5mwUTAE1WcLA2uR5qZ7t5iE&location=" +
+            address;
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            // After data comes back from the request
+            .then(function (response) {
+                console.log(queryURL);
+
+                //   console.log(response);
+                // storing the data from the AJAX request in the latLng variable
+                var latLng = response.results[0].locations[0].latLng;
+                console.log(latLng);
+
+            });
+
+
+
+    });
 });    
