a813f24585d1ae83031ee27fe495dc59257edcbe
Swarupa Popuri <swarupakarnam@yahoo.com>
2020-02-26 19:54:30 -0500

    Pushing dashboard add location

--- public/js/foodTruckLogic.js
+++ public/js/foodTruckLogic.js
@@ -1,4 +1,5 @@
 
+
 $(document).ready(function () {
   $("#nav-placeholder").load("nav.html");
 
@@ -16,6 +17,26 @@ $(document).ready(function () {
   $("#submitTruck").on("click", function (event) {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
+    var truckName = $("#foodTruckName").val().split(" ");
+      truckName = truckName.join("-");
+      console.log(truckName);
+      console.log("I am in ratings");
+    
+      // Constructing a queryURL using the address captured from the Address input field in the HTML
+      var yelpQuery = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/"+truckName+"/reviews"
+      var apiKey = "Bearer vONi09eF3JBM_C8djFa4wUnLta0Zmk331AT-PQ2-FNCFRND6BEeVZ5xtOCVeCvQViRhvegq23ZliF4kmyYTgSZNZ4gGBqICgX5KUdledrIBOpuu_uq5s1xs94XdUXnYx"
+    
+      $.ajax({
+          url: yelpQuery,
+          method: "GET",
+          headers: {
+              Authorization: apiKey
+          }
+      }).then(function (response) {
+          console.log(response.reviews[0]);
+          // console.log response;
+      });    
+
     var url = document.location.href,
       params = url.split('?')[1].split('&'),
       data = {}, tmp;
